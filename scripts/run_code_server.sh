5e25ddc2a4143f0f54222dfd98c065e5f302befd
fundev <farfarfun@qq.com>
2024-10-09 18:33:33 +0800

    add

--- scripts/run_code_server.sh
+++ scripts/run_code_server.sh
@@ -1,7 +1,7 @@
 
 #  ps -ef | grep code
 #curl -fsSL https://code-server.dev/install.sh | sh
-nohup code-server --config configs/code-server.yaml  >>./logs/darkdev/darkdev-$(date +%Y-%m-%d).log 2>&1 &
+nohup code-server --config /farfarfun/funenv/configs/code-server.yaml  >>/farfarfun/funenv/logs/funenv/darkdev-$(date +%Y-%m-%d).log 2>&1 &
 
 #nohup natapp -authtoken=595b08267b73d098 >>logs/code-server/code-server-$(date +%Y-%m-%d).log 2>&1 &
 #tail -f logs/code-server/code-server-$(date +%Y-%m-%d).log
